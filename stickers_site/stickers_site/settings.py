b575a4c4a6e19c5d041b6d37ed34646aef91ce27
fs.anvr <fs.anvr@gmail.com>
2022-06-18 22:23:47 +1000

    the same

--- stickers_site/stickers_site/settings.py
+++ stickers_site/stickers_site/settings.py
@@ -131,12 +131,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 
 MEDIA_ROOT = f'{BASE_DIR}/media'
 MEDIA_URL = '/media/'
